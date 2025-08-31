3fa98f7799ea30cb3cda17ed81a9929b385ce2e2
osotsia <osotsi.a@gmail.com>
2025-08-31 13:12:41 -0400

    sensitivity

--- src/analyses/sobol/index.js
+++ src/analyses/sobol/index.js
@@ -5,7 +5,7 @@ export default {
     title: 'Sobol Indices (True Model)',
     component: SobolChart,
     layout: 'default',
-    explanation: 'Sobol indices measure how much of the output variance is dependent on each input parameter, i.e. sensitivity, separating direct effects (S1) from total effects including interactions (ST).',
+    explanation: 'Sobol indices measure sensitivity, i.e. how much of the output variance is dependent on each input parameter, separating direct effects (S1) from total effects including interactions (ST).',
     props: {
         yKey: 'param',
         xKeys: ['S1', 'ST'],
