c404063472e927d12bbf62ec6a88ca1e6514e8c6
osotsia <osotsi.a@gmail.com>
2025-09-01 14:15:21 -0400

    surrogate

--- src/analyses/model-card/index.js
+++ src/analyses/model-card/index.js
@@ -5,7 +5,7 @@ export default {
     title: 'Model Card (Surrogate)',
     component: ModelCardDisplay,
     layout: 'default',
-    explanation: 'We train a surrogate when the original model is slow or otherwise costly to evaluate. Below: key metadata and performance metrics for the XGBoost surrogate model.',
+    explanation: 'We train a surrogate when the original model is slow or otherwise costly to evaluate. Below: key metadata and performance metrics.',
     props: {},
     mapper: ($dataStore, $workingState) => ({
         data: $dataStore.model_card || {}
